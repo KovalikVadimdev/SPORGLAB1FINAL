00e1496be03a5177810c20bf2b2fd50b23089f73
Kovalik Vadim <vadym.kovalyk@nure.ua>
2023-11-02 00:19:32 +0200

    Completed laboratory work No. 1 on SProg

---
+++ SPROGLAB1FINAL/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SPROGLAB1FINAL.rc
+//
+#define IDCANCEL2                       3
+#define IDS_STRING101                   101
+#define IDS_STRING102                   102
+#define IDD_DIALOG1                     102
+#define IDS_STRING103                   103
+#define IDD_DIALOGBAR                   103
+#define IDR_MENU1                       104
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define ID_MENU_ABOUTTHEPROGRAM         40001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
