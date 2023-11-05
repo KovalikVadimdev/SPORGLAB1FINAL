d9b77077cbc6ecdd5ea7364c82905b16e6393a8b
Kovalik Vadim <vadym.kovalyk@nure.ua>
2023-11-05 23:21:12 +0200

    Second difficulty

--- SPROGLAB1FINAL/resource.h
+++ SPROGLAB1FINAL/resource.h
@@ -9,17 +9,30 @@
 #define IDS_STRING103                   103
 #define IDD_DIALOGBAR                   103
 #define IDR_MENU1                       104
+#define IDD_DIALOG2                     108
+#define IDR_MENU2                       110
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
+#define IDC_EDIT1                       1003
 #define ID_MENU_ABOUTTHEPROGRAM         40001
+#define ID_MENU_SECOONDDIALOGBOX        40002
+#define ID_MENU_SECONDDIALOGBOX         40003
+#define ID_FILE_CREATE                  40004
+#define ID_FILE_EDIT                    40005
+#define ID_NEWMENUITEM1_GIT             40006
+#define ID_NEWMENUITEM1_YOUTUBE         40007
+#define ID_NEWMENUITEM2_VADIM           40008
+#define ID_NEWMENUITEM2_DIMA            40009
+#define ID_MENU_CHANGEMENU              40010
+#define ID_FILE_RETURNOLDMENU           40011
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
-#define _APS_NEXT_COMMAND_VALUE         40002
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_COMMAND_VALUE         40012
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
