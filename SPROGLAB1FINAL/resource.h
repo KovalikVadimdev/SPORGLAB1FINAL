4cc6c2003751e70a3ebfa076e72294148c606efd
KovalikVadimdev <124208880+KovalikVadimdev@users.noreply.github.com>
2023-11-09 18:25:40 +0200

    Add files via upload

--- SPROGLAB1FINAL/resource.h
+++ SPROGLAB1FINAL/resource.h
@@ -6,11 +6,16 @@
 #define IDS_STRING101                   101
 #define IDS_STRING102                   102
 #define IDD_DIALOG1                     102
+#define IDD_ABOUT                       102
 #define IDS_STRING103                   103
 #define IDD_DIALOGBAR                   103
+#define IDD_QUESTIONS                   103
 #define IDR_MENU1                       104
 #define IDD_DIALOG2                     108
+#define IDD_READ                        108
 #define IDR_MENU2                       110
+#define IDD_                            111
+#define IDD_MESSAGE                     111
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
 #define IDC_EDIT1                       1003
@@ -27,10 +32,10 @@
 #define ID_FILE_RETURNOLDMENU           40011
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_RESOURCE_VALUE        113
 #define _APS_NEXT_COMMAND_VALUE         40012
 #define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
